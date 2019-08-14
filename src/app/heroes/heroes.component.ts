d889ee06d82b76dfd42f1603493acbc2cd619461
Dracheva Yana <Yana.Dracheva@indusoft.ru>
2019-08-14 17:39:56 +0300

    4th Tour of heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+/*import { HEROES } from '../mock-heroes';*/
+import { HeroService } from '../hero.service';
 
 
 @Component({
@@ -14,16 +15,24 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   
   };
-  heroes = HEROES;
+  heroes: Hero[];
+  /*heroes = HEROES;*/
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-}
-  
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+  /*getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }*/
 
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   

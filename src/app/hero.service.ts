a2e10dc036df9c6b429d685f142e2555613c5d00
Dracheva Yana <Yana.Dracheva@indusoft.ru>
2019-08-15 13:02:48 +0300

    TOH 4 ready

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,9 @@ export class HeroService {
     return HEROES;
   }*/
   getHeroes(): Observable<Hero[]> {
+     // TODO: send the message _after_ fetching the heroes
+  this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,7 +24,7 @@ export class HeroesComponent implements OnInit {
   }
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe((heroes: Hero[]) => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
 }
   /*getHeroes(): void {
     this.heroService.getHeroes()

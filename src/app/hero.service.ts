2ac4296f3dadf2fa0fc85a25954fa8073145efa2
Dracheva Yana <Yana.Dracheva@indusoft.ru>
2019-08-19 18:09:26 +0300

    TOH is ready

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -51,7 +51,27 @@ export class HeroService {
       catchError(this.handleError<Hero>('addHero'))
     );
   }
+  /** DELETE: delete the hero from the server */
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
 
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+  /* GET heroes whose name contains search term */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
   constructor(
     private http: HttpClient,
     private messageService: MessageService) { }
